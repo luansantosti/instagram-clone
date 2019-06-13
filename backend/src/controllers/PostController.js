90fdf6a6f414d53b3de8ac902d72a7912b09e40e
luan <luanjr21@gmail.com>
2019-06-13 08:47:27 -0300

    Backend ready!

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,14 +1,50 @@
 const Post = require('../models/Post')
+const sharp = require('sharp')
+const path = require('path')
+const fs = require('fs')
+
 
 module.exports = {
-  async index(req, res) {
 
+  // GET LIST OF ITEMS
+  async index(req, res) {
+    const posts = await Post.find().sort('-createdAt')
+
+    return res.json(posts)
   }, 
 
+  // CREATE POST
   async store(req, res) {
-    
+    const { author, place, description, hashtags } = req.body;
+    const { filename: image } = req.file;
 
-    return res.json({ ok: true})
+    // TRANSFORM PNG TO JPG
+    const [ name ] = image.split('.')
+    const fileName = `${name}.jpg`
+
+    // IMAGE RESIZE
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(
+        path.resolve(req.file.destination, 'resized', fileName)
+      )
+
+    // DELETE ORIGINAL IMAGE
+    fs.unlinkSync(req.file.path)
+
+    // SEND TO DATABASE
+    const post = await Post.create({
+      author, 
+      place,
+      description,
+      hashtags,
+      image: fileName,
+    })
+
+    req.io.emit('post', post)
+
+    return res.json(post)
   }
 
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
 
 const app = express()
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 mongoose.connect('mongodb+srv://luansantos:luansantos@cluster0-pqlh1.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true, 
 })
 
+app.use((req, res, next) => {
+  req.io = io
+
+  // ALLOW THE NEXT ITEMS TO BE EXECUTED
+  next()
+})
+
+// ALLOW ANY APP ACCESS THE SERVER
+app.use(cors())
+
+// ROUTE FOR FRONT ACCESS IMAGES
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+
 app.use(require('./routes'))
 
-app.listen(3333)
+server.listen(3333) 
